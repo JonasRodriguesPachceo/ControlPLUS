cdb6ffd71c48d38cfc3dc6e29dd9c92e70bc947b
robsonnsbr <robsonnsbr@gmail.com>
2025-11-13 11:00:21 -0300

    chore: initial commit with project files copied from reference project

---
+++ resources/views/impressao_pedido/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app', ['title' => 'Controle de Impressão'])
+@section('content')
+<div class="mt-3">
+    <div class="row">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-2">
+                        <i class="ri-printer-line text-primary" style="font-size: 70px;"></i>
+                    </div>
+                    <div class="col-md-10">
+                        <br>
+                        <h5>Tela de impressão de pedidos</h5>
+                        <h4 class="text-primary">{{ $item->descricao }}</h4>
+                    </div>
+                </div>
+                <hr>
+                <div class="row logs">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<input type="hidden" id="impressora_id" value="{{ $item->id }}">
+@endsection
+@section('js')
+<script type="text/javascript" src="/js/controle_impressao_pedido.js"></script>
+@endsection
